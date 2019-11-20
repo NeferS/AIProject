54be79953f40ade2cef4bb66bf1f8670b4c3b603
mastercommander <mastercommander@160.97.137.222>
2019-11-20 02:04:27 +0100

    Prima implementazione GameEngine

--- src/representations/GameEngine.java
+++ src/representations/GameEngine.java
@@ -1,16 +1,22 @@
 package representations;
 
+import java.util.List;
+
 public interface GameEngine {
 	
-	/**Crea un nuovo nodo a partire dalla configurazione this.
-	 * @param m la mossa da eseguire sulla configurazione
-	 * @return la rappresentazione di un nuovo nodo creato applicando la mossa m alla configurazione this
-	 */
-	RepresentationNode result(RepresentationNode n, String m);
+
 	/**Calcola e restituisce tutte le mosse valide a partire dalla configurazione passata come parametro.
 	 * @param configuration la configurazione di partenza
 	 * @return un array di mosse valide
 	 */
-	RepresentationNode[] validActions(RepresentationNode configuration);
+	public List<RepresentationNode> validActions(RepresentationNode configuration);
+
+	public void start(Color color);
+	
+	public RepresentationNode getCurrentBoardState();
+	
+	public void playerMakeMove(RepresentationNode boardState);
+	
+	public RepresentationNode enemyMakeMove(String encodedMove);
 	
 }
--- src/strategies/IHeuristic.java
+++ src/strategies/IHeuristic.java
@@ -2,7 +2,7 @@ package strategies;
 
 import representations.RepresentationNode;
 
-/**Definisce una strategia che il giocatore automatico può seguire sulla rapprentazione dello
+/**Definisce una strategia che il giocatore automatico puï¿½ seguire sulla rapprentazione dello
  * spazio degli stati R.
  * @param R la rappresentazione dello spazio degli stati
  * @author Vincenzo Parrilla
@@ -13,12 +13,6 @@ public interface IHeuristic {
 	 * @param node la configurazione da valutare
 	 * @return il valore della funzione euristica h
 	 */
-	double h(RepresentationNode node);
-	
-	/*Potrebbe essere necessario valutare se cambiare strategia o meno in base alla configurazione del nodo ricevuto.*/
-	/**Restituisce la migliore strategia attuabile a partire dalla configurazione ricevuta in input,
-	 * @param configuration la configurazione attuale
-	 * @return la prossima strategia (può coincidere con this)
-	 */
-	IHeuristic valuateStrategy(RepresentationNode configuration);
+	public double h(RepresentationNode node);
+
 }
